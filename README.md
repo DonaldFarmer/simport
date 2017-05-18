d7425e26b14099ce5ef4860136afcd241ef26667
Donald Farmer <donaldfarmer@gmail.com>
2017-05-18 09:52:24 -0400

    changes from testing

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # simport
 Selenium command file to automate the importing of products into Shopify
+
+Install python webdriver package
+https://pypi.python.org/pypi/selenium
+
+```python
+sudo apt-get install python-pip
+
+pip install selenium
+
+cd ~/dev/cma
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.16.0/geckodriver-v0.16.0-linux64.tar.gz
+
+tar -xvzf geckodriver*
+
+chmod +x geckodriver
+
+
+sudo mv geckodriver /usr/bin
+
+?????export PATH=$PATH:/~/dev/cma/geckodriver
+```
+
+
+Download latest chrome driver, unzip into development folder where the script is located
+http://chromedriver.storage.googleapis.com/index.html
+
+
+If you want to get the Selenium IDE
+https://addons.mozilla.org/en-US/firefox/addon/selenium-ide/
+
--- simport.py
+++ simport.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: latin-1 -*-
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,83 +15,104 @@ configure
 login
 navigate to import page
 """
- 
-url = ""
-username = ""
-password = ""
-filepath = ""
 
+url_prod = "https://sandbox-joule.myshopify.com/admin/products"
+url_lang = "https://sandbox-joule.myshopify.com/admin/apps"
+username = "don.farmer@cma.ca"
+password = ""
+filepath_prod = "/home/donald/Documents/product_template.csv"
+filepath_lang = "/home/donald/Documents/lang.csv"
 
 def init_driver():
     driver = webdriver.Firefox()
     driver.wait = WebDriverWait(driver, 5)
     return driver
- 
- 
+
+
 def checkfile(): # check and see if a valid file exists at upload location
   pass
+
 
 def open(driver, uri): # navigates to webpage
   driver.get(uri)
 
-def type(driver, type, name, value):
-  if type == "id":
-    elem = driver.find_element_by_id(name) 
 
+def type(driver, typ, name, val):
+  if typ == "id":
+    elem = driver.find_element_by_id(name) 
+  elif typ == "class":
+    elem = driver.find_element_by_class_name(name)
   try:
-    elem.send_keys(value)
+    elem.send_keys(val)
   except:
-    print('unable to send keys {0} {1} {2}'.format(type, name, value))
+    print('unable to send keys {0} {1} {2}'.format(typ, name, val))
+
+
 
 def clickandwait(): 
   pass
 
-def click(driver, type, value)
-  if type = "id":
+
+def click(driver, type, value):
+  if type == "id":
     elem = driver.find_element_by_id(value) 
   elif type == "name":
     elem = driver.find_element_by_name(value) 
   elif type == "xpath":
     elem = driver.find_element_by_xpath(value) 
-
+  elif type == "linktxt":
+    elem = driver.find_element_by_link_text(value)
   try:
     elem.click()
-  except ElementNotVisibleException:
+  except:
     print('unable to click {0} {1}'.format(type, value))
 
 
- 
+def navigate(driver, val):
+  driver.url(val)
+
+
 if __name__ == "__main__":
   driver = init_driver()
 
   open(driver, "https://sandbox-joule.myshopify.com/admin/auth/login")
+  WebDriverWait(driver, 5)
+
+
   type(driver, "id", "login-input", "don.farmer@cma.ca")
   type(driver, "id", "password", "")
-  click(driver, "name", "commit")
-  click(driver, "xpath","//div[@id='NextNavigation']/div/nav/ol/li[3]/a/span")
-  click(driver, "id", "ui-popover-activator--2")
-  click(driver, "xpath", "(//button[@name='button'])[7]")
+  type(driver, "class", "dialog-btn", Keys.RETURN)
+  WebDriverWait(driver, 3)
+
+  # navigate(driver, "https://sandbox-joule.myshopify.com/admin/products") # s/b on this page now
+
+  click(driver, "linktext","Products")
+  WebDriverWait(driver, 2)
+  click(driver, "linktext", "Import")
+
+  click(driver, "id", "overwrite_existing_products")
   type(driver, "id", "csv_input_field", "/home/donald/Documents/product_template.csv")
   click(driver, "id","upload-file-btn")
 
 
-  open(driver, "https://sandbox-joule.myshopify.com/admin/apps")
-  click(driver, "link", "langify")
-  click(driver, "link", "Français")
-  clickandwait(driver, "link", "Import translations...")
-  click(driver, "link", "import-intro-next")
-  click(driver, "link", "form_themes_0")
-  click(driver, "link", "import-step-1-next")
-  click(driver, "link", "form_categories_0")
-  click(driver, "link", "form_categories_2")
-  click(driver, "link", "form_categories_3")
-  click(driver, "link", "form_categories_4")
-  click(driver, "link", "form_categories_5")
-  click(driver, "link", "import-step-2-next")
-  click(driver, "link", "import-step-3-next")
-  click(driver, "link", "form_import_csv")
-  #type(driver, "id", "csv_input_field", "/home/donald/Documents/lang.csv")
-  #click(driver, "id","upload-file-btn")
+  # need to wait until previous upload is finished
+  # navigate(driver, "https://sandbox-joule.myshopify.com/admin/apps") 
+  # click(driver, "link", "langify")
+  # click(driver, "link", "Français")
+  # clickandwait(driver, "link", "Import translations...")
+  # click(driver, "link", "import-intro-next")
+  # click(driver, "link", "form_themes_0")
+  # click(driver, "link", "import-step-1-next")
+  # click(driver, "link", "form_categories_0")
+  # click(driver, "link", "form_categories_2")
+  # click(driver, "link", "form_categories_3")
+  # click(driver, "link", "form_categories_4")
+  # click(driver, "link", "form_categories_5")
+  # click(driver, "link", "import-step-2-next")
+  # click(driver, "link", "import-step-3-next")
+  # click(driver, "link", "form_import_csv")
+  # #type(driver, "id", "csv_input_field", "/home/donald/Documents/lang.csv")
+  # #click(driver, "id","upload-file-btn")
 
   #time.sleep(5)
   #driver.quit()
